a95fa811937788aaea1d892860b83e50f4c3b6ba
Krave <krave@qq.com>
2021-03-10 16:41:35 +0800

    finished

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,11 @@
 	function handleClick() {
 		count += 1;
 	}
+	$: doubled = count * 2;
 </script>
 
 <button on:click={handleClick}> Clicked {count} time{count === 1 ? "" : "s"}</button>
+<p>{count} doubled is {doubled}</p>
 
 <style>
 </style>
