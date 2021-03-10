d21536303abebaba8cf52bd34459fe55a563c3b1
Krave <krave@qq.com>
2021-03-10 15:39:43 +0800

    finished

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,11 @@
 <script>
-	export let name;
-	name = "Krave";
+	let count = 0;
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button on:click={handleClick}> Clicked {count} time{count === 1 ? "" : "s"}</button>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		/* text-transform: uppercase; */
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
